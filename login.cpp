a487f08a2673f60dfd4071c111835e3ee1695cba
Laaazy <38176577+Laaazy@users.noreply.github.com>
2018-09-05 19:42:00 +0800

    Create login.cpp

---
+++ login.cpp
@@ -0,0 +1,15 @@
+/*
+void login(){
+    sendMsg("login");
+    char userID[20]=ui->userID->Text();
+    char passWord[20]=ui->passWord->Text();
+    sendMsg(userID);
+    sendMsg(passWord);
+    if(recvMsg().equals("correct")){
+        goto main ui;
+    }
+    else{
+        show warning window "not correct"
+    }
+}
+*/
